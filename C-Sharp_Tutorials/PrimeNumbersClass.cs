147ef0261b9e8011a510c39808514416a2211940
Venkat Reddy <vry6576@gmail.com>
2020-05-08 09:44:50 -0500

    Changes made in PrimeNumbersClass

--- C-Sharp_Tutorials/PrimeNumbersClass.cs
+++ C-Sharp_Tutorials/PrimeNumbersClass.cs
@@ -6,8 +6,24 @@ namespace C_Sharp_Tutorials
 {
     class PrimeNumbersClass
     {
-        int[] i = new int[100];
+        public static void PrimeNumbersClassMethod()
+        {
 
+            int[] PrimeNumbers = new int[100];
 
+            int i = 0;
+            while (i <=100)
+            {
+                int j = 0;
+                while (j<=100)
+                {
+                    
+
+                    j++;
+                }
+
+                i++;
+            }
+        }
     }
 }
